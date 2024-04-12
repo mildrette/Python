d3477974c9d866c1b3d55d2527dc66d4abb27fae
Mildred <99267408+mildrette@users.noreply.github.com>
2024-04-12 09:22:54 +0100

    python day 3

---
+++ Basic.py
@@ -0,0 +1,38 @@
+# import keyword
+# print("The list of keywords is : ")
+# print(keyword.kwlist)
+
+
+# # True, False, None Keywords
+
+# print( False == 0)
+# print(True == 1)
+
+# print(True + True + True)
+# print(True + False + False)
+
+# print(None == 0)
+# print(None == [])
+
+
+
+# and, or, not, in, is In Python
+
+# print(True or False)
+# print( True and False)
+print(not True)
+
+if "s" in "mildredfonka":
+    print("m is part of the name")
+else:
+    print('s is not part of the name')
+    for m in "mildredfonka":
+        print(m, end=" ")
+
+        # print("\r")
+        # print(' ' is ' ')
+        print({} is {})
+
+
+
+# Iteration Keywords – for, while, break, continue
