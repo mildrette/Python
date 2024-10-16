23021d2d54767ca88a3afb916c93edf92193cfbf
mildrette <mildredfonka1@gmail.com>
2024-10-16 13:33:10 +0100

    update to python lessons

--- Basic.py
+++ Basic.py
@@ -1,38 +1,193 @@
-# import keyword
-# print("The list of keywords is : ")
-# print(keyword.kwlist)
+# variables
+name = "John Smith"
+age = 20 
+is_new = True
+print()
+
+# how to receive input from a users [input] function
+# name = input('what is your name? ')
+# color = input('what is your favorite color? ')
+# print(name + ' loves '  +  color)
+
+# ask question calculate and give answers, int is short fo interger and str is short for string
+# converting a string to interger
+# int() for converting a string to and interger
+# float() for converting a string to a float or a number with a decimal point
+# bool() for converting a string to boolen
+
+# birth_year = input('Birth year: ')
+# age = 2024 - int(birth_year)
+# print(type(age))
+
+# weight = input('What is your weight: ') 
+# kg = int(weight) * 0.45
+# print(kg)
 
 
-# # True, False, None Keywords
+# using quotes
 
-# print( False == 0)
-# print(True == 1)
+# lessons = "learning python for beginner's"
+# print(lessons)
 
-# print(True + True + True)
-# print(True + False + False)
+# multiple line
 
-# print(None == 0)
-# print(None == [])
+# lesson = '''
+# hello there,
+# i am learing multiple quotes in python '''
+# print(lesson)
 
 
 
-# and, or, not, in, is In Python
+# learning = 'python for "Beginners"'
+# another = learning[:]
+# print(learning[0:7]) #it takes from the beginng
+# print(learning[0:]) #it takes from the beginng
+# print(learning[-3]) #it takes from the end
+# print(another)
 
-# print(True or False)
-# print( True and False)
-print(not True)
+# name = 'Jenisfer'
+# print(name[1:-1])
 
-if "s" in "mildredfonka":
-    print("m is part of the name")
+# formated strings in python
+
+# firs_name = 'mildred'
+# last_name = 'fonka'
+# message = firs_name + ' [ ' +  last_name +'] is learnig python'
+# msg = f'{firs_name} {last_name} is learning python'
+# print(msg) 
+
+# Python Strings
+
+# Course = 'Python for beginners'
+# print(len(Course))
+# # function for converting character from lower case to uppercase
+# Course = 'Python for beginners'
+# print(Course.upper())
+# print(Course.lower())
+# print(Course.find('b'))
+# print(Course.find('n'))
+# print(Course.title())
+# #  replacing a character
+# print(Course.replace('beginners', ' Absolute Beginners'))
+
+# # to know if a string contains a word
+
+# learn = 'I am learning python today'
+# print("I " in learn)
+
+# #  mathomatical operators suported in python
+
+# print(10 ** 4)
+
+# X = 4
+# X = X + 10
+# print(X)
+
+# x = 20
+# x //= 56
+# print(x)
+
+# x = 10 + 3 * 2 ** 2 % 22
+# print(x)
+
+
+# m = (2 + 3) * 10 -3
+# print(m)
+
+
+# Python = 10.5
+# # print(round(Python))
+# print(abs(-10.5))
+
+
+
+# import math
+
+# math.ceil(2.9)
+# print(math.ceil(10.8))
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  if statement in python
+
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It's a hot day")
+    print("Enjoy your day")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear Worm Cloths")
 else:
-    print('s is not part of the name')
-    for m in "mildredfonka":
-        print(m, end=" ")
-
-        # print("\r")
-        # print(' ' is ' ')
-        print({} is {})
+    print("It's a bueatifull day, let's go fro a walk")
 
 
+# if, elif, and else statement
+price = 1000000
+House_is_1Million = False
+House_isnot_1Million = True
 
-# Iteration Keywords – for, while, break, continue
+if House_is_1Million:
+    print('deposit 10% to show your serious')
+elif House_isnot_1Million:
+    print('how much do you think you can give')
+else:
+    print('deposite 20%')
+
+
+# if and else statement
+price = 1000000
+has_Good_credit = True
+
+if has_Good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f"Down Payment:${down_payment}")
+
+# logicla operators
+
+has_high_income = False
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print("eligible for Loan")
+
+has_High_income = False
+has_GOod_credit = True
+
+if has_High_income or has_GOod_credit:
+    print("eligible for Loan")
+
+
+# 
+
+has_good_credit = True
+has_criminal_Record = True
+
+
+if has_good_credit and not has_criminal_Record:
+    print('eligible for loan')
+else:
+    print('not eligible for loan')
+
+
+#  comparison operator
+
+Temperature = 30
+
+if Temperature > 30:
+    print("it's a hot day")
+else: 
+    print("It's not a hot day")
