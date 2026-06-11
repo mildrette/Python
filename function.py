065e6f8de9d19e4b11ea275e4af1ae6223749f44
Mildrette <mildredfonka1@gmail.com>
2026-06-11 17:34:44 +0100

    Lesson 6: Functions - define once, call anytime

---
+++ function.py
@@ -0,0 +1,25 @@
+# lesson 6: Functions
+
+def makeTea():
+    print("Boiling water")
+    print("put milk in the cup")
+    print("add sugar and overtine")
+    print("add the boiling water")
+    print("Adding lemon of you want")
+    print("stir and enjoy you tea")
+ 
+makeTea()
+
+
+name = "Mildred"
+encouragement = " Hey girl, you are doing it after all and it's great right?"
+goal = "I hope you remember the goal here, is to be the version of yourself we alway think about"
+
+def myDailyAffirmation():
+    print(name)
+    print(encouragement)
+    print(goal)
+myDailyAffirmation()
+
+for this in range(3):
+    myDailyAffirmation()
