c2d6e8eeacbeee0939865f363c05631b583ded7c
Mildrette <mildredfonka1@gmail.com>
2026-06-11 15:59:21 +0100

    lesson 3, datatypes, and we learn the 3 basic data types, string, integer, and boolean

--- Basic.py
+++ Basic.py
@@ -10,3 +10,14 @@ print(myMood)
 
 # data types
 
+# String - has quotes
+name = "Mildred"
+print(name)
+
+# integer - just numbers, whole numbers
+age = 30
+print(age)
+
+# booleans - is just true or false
+isLearning = True
+print(isLearning)
