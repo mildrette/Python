ca2a5d08a9a020910522b815ee7c60abd2ab37ec
Mildrette <mildredfonka1@gmail.com>
2026-06-11 16:22:42 +0100

    made a mini project with all i have learned so far, made a python ID card

--- Basic.py
+++ Basic.py
@@ -21,3 +21,29 @@ print(age)
 # booleans - is just true or false
 isLearning = True
 print(isLearning)
+
+# float - has a decimal point
+kilometers = 5.5
+print(kilometers)
+
+# if/else statements
+mildredIsLearning = True
+
+if mildredIsLearning:
+    print("Mildred is learning Python today")
+else:   print("Mildred is not learning Python today")
+
+# Mini Project from all i have learned so far
+# my Python ID Card
+
+name = "Mildred"
+age = 30
+city = "Bamenda"
+isLearningPython = True
+mood = "happy"
+print("My name is " + name + " and I am  " + str(age) + "years old. today i am learning python " + str(isLearningPython) + " and I am so happy i can understand what i am learning so far.  i am presently living in " + city)
+
+if mood == "happy":
+    print("keep going! Good things are ahead of you")
+else:
+    print("Breathe and reset, You got this, you can so this")
