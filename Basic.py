2dd389222129ba757b75de0a3dafda45964e709d
Mildrette <mildredfonka1@gmail.com>
2026-06-11 16:46:24 +0100

    made a mini project with all i have learned so far, here the project was on loops and all the other things i had learn incooporated together

--- Basic.py
+++ Basic.py
@@ -47,3 +47,38 @@ if mood == "happy":
     print("keep going! Good things are ahead of you")
 else:
     print("Breathe and reset, You got this, you can so this")
+
+
+# Loops
+for i in range(10):
+    print("i will stay consistent with my learning and new habits, so i become a besst version of myself. yes i can and i will")
+
+for habits in range(5):
+    print("eating healthy everyday")
+
+for consistency in range(20):
+    print("we got this, yes we do")
+
+# loops mini project
+# my daily routine loops
+
+name = "Mildred"
+for morningRoutine in range(1):
+    print("wake up")
+    print("Brush Teeth")
+    print("Eat Breakfast")
+    print("Open python and study")
+
+
+affirmation = "I am Hopefull and i will stay Hopefull, It's better than being pitifull about myself everyday"
+
+for myLoop in range(5):
+    print(affirmation)
+
+
+studyToday = True
+if studyToday:
+    print("Great job Mildred, you showed up today and i proud of you")
+else:
+    print("it's not the end, you till have time to do it, just believe in youself and do not give up. we got this, we will have hard days but we can do this okay, do not reture to the sorrowfull you. we have a plan and a goal to reach, it's okay to fall as long as we raise and continue ")
+    
