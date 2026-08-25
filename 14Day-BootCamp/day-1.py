83e14a156af24f89110ef5191dcdcaa7aca25ad1
Mildrette <mildredfonka1@gmail.com>
2026-08-25 22:10:59 +0100

    14 days bootcamp for python

---
+++ 14Day-BootCamp/day-1.py
@@ -0,0 +1,7 @@
+name = "Astrid"
+age = 31
+learning = True
+language = "Python"
+print(name)
+
+print(f"Hello! my name is {name}, and i am learning {language} and i am {age} years old and am I actually welling to make this work? {learning}")
