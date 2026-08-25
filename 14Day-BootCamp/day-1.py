afe81598e7d5b0c1c7be32a2a26a46e402cb8d20
Mildrette <mildredfonka1@gmail.com>
2026-08-25 22:50:24 +0100

    learned input that is question and answer in python

--- 14Day-BootCamp/day-1.py
+++ 14Day-BootCamp/day-1.py
@@ -1,9 +1,12 @@
-name = "Astrid"
+name = input("what is your name")
 age = 31
 learning = True
 language = "Python"
 print(name)
-
 print(f"Hello! my name is {name}, and i am learning {language} and i am {age} years old and am I actually welling to make this work? {learning}")
-
 print(f"what's your name? {name}")
+
+
+has_computer = input("do you have a computer?")
+
+print(f"{has_computer} Its a very nice one")
