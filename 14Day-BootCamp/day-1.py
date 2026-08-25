edcb6facbfe14369846b5ea026276ff70b4fa88a
Mildrette <mildredfonka1@gmail.com>
2026-08-25 22:30:20 +0100

    print

--- 14Day-BootCamp/day-1.py
+++ 14Day-BootCamp/day-1.py
@@ -5,3 +5,5 @@ language = "Python"
 print(name)
 
 print(f"Hello! my name is {name}, and i am learning {language} and i am {age} years old and am I actually welling to make this work? {learning}")
+
+print(f"what's your name? {name}")
