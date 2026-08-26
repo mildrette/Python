362a7943532d0e787a955d7aa309b43e5b17d0e3
Mildrette <mildredfonka1@gmail.com>
2026-08-26 21:53:39 +0100

    Complete Python Day 1 numbers lesson

--- 14Day-BootCamp/day-2.py
+++ 14Day-BootCamp/day-2.py
@@ -1,1 +1,33 @@
 # Lesson 1 — Numbers
+
+age = 31
+print(age - 2 )
+
+print(age * 4)
+
+print(age + 5)
+
+print(age / 2)
+
+age_in_2027 = age + 1
+
+print(age_in_2027)
+
+# note to self. in python when wanting to use input on a number instead of a text, use int infront of the input like this Int(input(then value in here))
+
+year = int(input("what age are you? "))
+
+next_year_age = year + 1
+
+print(year + 5)
+
+print(f"hello world i am {year} old today and next year i will be {next_year_age} year old. thank you")
+
+# little challenge
+
+present_age = int(input("How olded are you today?? "))
+print(present_age)
+
+age_in_five_year = present_age + 5
+
+print(f"In five year, You will be {age_in_five_year} old.")
