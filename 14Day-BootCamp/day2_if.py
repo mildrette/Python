6264563913fd8c22fa1e5689f4a8353b32bce04e
Mildrette <mildredfonka1@gmail.com>
2026-08-28 23:47:27 +0100

    done with the equal to sign

--- 14Day-BootCamp/day2_if.py
+++ 14Day-BootCamp/day2_if.py
@@ -1,4 +1,5 @@
 #  Day 3 part 2 — Making Decisions with if statement
+# lessons one the : == equal to sign
 raining = True
 
 if raining:
@@ -8,3 +9,22 @@ age = 30
 
 if age >= 30:
     print(f"I am {age} years old")
+
+if age == 30:
+    print(f"i am an adult")
+
+weather = "sunny"
+
+if weather == "rainny":
+    print("It's raining")
+
+if weather == "sunny":
+    print("its sunny")
+
+password = "python123"
+
+if password == "python123":
+    print("password is correct")
+
+
+# the Not Equal to sign
