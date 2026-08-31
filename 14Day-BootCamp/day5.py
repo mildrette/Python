4b3a7309e61277818c2520966cef328893ae3476
Mildrette <mildredfonka1@gmail.com>
2026-08-31 21:41:52 +0100

    loops seperately

---
+++ 14Day-BootCamp/day5.py
@@ -0,0 +1,14 @@
+# loops
+
+animals = ["Cat", "Dog", "Pig", "Rat", "Rabbit"]
+
+for animal in animals:
+    print("I Love", animal)
+    print( "A", animal, "is an animal")
+    print("I think I would like to own a", animal)
+
+Fruits = ["apples", "Pears", "Bananas", "Oranges", "PinApples"]
+
+for fruit in Fruits:
+    print("I like", fruit)
+    print("This is a fruit")
