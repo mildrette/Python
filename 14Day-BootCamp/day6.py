f39c72454fe5e27f5f7b4392f961e887d829dbcc
Mildrette <mildredfonka1@gmail.com>
2026-09-01 21:51:40 +0100

    next tiny dictionary lesson will be removing information

--- 14Day-BootCamp/day6.py
+++ 14Day-BootCamp/day6.py
@@ -1,1 +1,20 @@
 # Here, I showed up again. Day two of me showing up 
+
+# Todays lessons is Dictionary
+
+person = {"age": 31,
+          "name": "Mildred",
+          "lesson":"Python",
+          "Country":"Cameroon"
+          }
+person["job"] = "Developer"
+person["hobby"] = "Singing"
+
+person["job"] = "Banker"
+person["hobby"] = "Dancing"
+
+print(person["name"])
+
+print(person)
+
+
